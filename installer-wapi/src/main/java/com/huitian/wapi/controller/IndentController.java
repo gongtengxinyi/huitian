10ea27ff6c9ff472fc590d192ada372c08fcf8f4
dingjianlei <1251272104@qq.com>
2017-09-25 16:29:04 +0800

    人

--- installer-wapi/src/main/java/com/huitian/wapi/controller/IndentController.java
+++ installer-wapi/src/main/java/com/huitian/wapi/controller/IndentController.java
@@ -6,6 +6,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 ///订单相关的controller
@@ -16,6 +17,15 @@ public class IndentController  {
 	@RequestMapping(value = "demo")
 	@ResponseBody
 	public String AssociatedinformationGet(HttpServletRequest request) {
+		return "login";
+	}
+	@RequestMapping(value = "getIndentListForEveryType")
+	@ResponseBody
+	public String getIndentListForEveryType(@RequestParam(required = false, defaultValue = "1", value = "page") int page, // 记录页数
+            @RequestParam(required = false, defaultValue = "6", value = "limit") int limit,
+            @RequestParam(required = true, defaultValue = "", value = "type") int type) {
+		
+		
 		return "login";
 	}
 }
